d25a89e3ce537e32f146fbbf568908b8c898d4fb
iinsue <hinsu93@naver.com>
2023-08-21 00:14:17 +0900

    navbar-action 위치 정하기

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 
 import Container from "@/components/ui/container";
-import MainNav from "./main-nav";
+import MainNav from "@/components/main-nav";
 import getCategories from "@/actions/get-categories";
+import NavbarActions from "@/components/navbar-actions";
 
 const Navbar = async () => {
   const categories = await getCategories();
@@ -15,6 +16,7 @@ const Navbar = async () => {
             <p className="font-bold text-xl">STORE</p>
           </Link>
           <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
