cc0092733ffecce64dec0508aa24386da41ca8ba
iinsue <hinsu93@naver.com>
2023-08-21 00:33:12 +0900

    button 에 스타일 추가하기

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { ShoppingBag } from "lucide-react";
+
+import Button from "@/components/ui/button";
+
 const NavbarActions = () => {
   return (
     <>
-      <div className="ml-auto flex items-center gap-x-4">Hello Actions</div>
+      <div className="ml-auto flex items-center gap-x-4">
+        <Button className="flex items-center rounded-full bg-black px-4 py-2">
+          <ShoppingBag size={20} color="white" />
+        </Button>
+      </div>
     </>
   );
 };
