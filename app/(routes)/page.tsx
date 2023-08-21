e0f9d822bcbf11f005dc67c0c58cdb155c927d6d
iinsue <hinsu93@naver.com>
2023-08-21 23:15:48 +0900

    Billboard 설정 추가

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,19 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("ee464c55-c9a7-4058-817d-1ce2698dec3b");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
