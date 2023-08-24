7af2d3aeca7db8699445e1aad2ef57f679eef395
iinsue <hinsu93@naver.com>
2023-08-24 22:48:32 +0900

    product 상세 연관 물품 추가

---
+++ components/gallery/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Image from "next/image";
+import { Tab } from "@headlessui/react";
+
+import { Image as ImageType } from "@/types";
+
+const Gallery = () => {
+  return (
+    <>
+      <div>Gallery</div>
+    </>
+  );
+};
+
+export default Gallery;
